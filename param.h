8507248281e8e4aee94bda3d61218695b6fe7ac2
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2019-06-07 13:13:29 +0430

    Amend SYSCALLMAX to 22 and HISTORY to 20

--- param.h
+++ param.h
@@ -11,6 +11,6 @@
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
-#define HISTORYMAX       10  // size of history
-#define SYSCALLMAX       23  // max number of syscalls in system
+#define HISTORYMAX       20  // size of history
+#define SYSCALLMAX       22  // max number of syscalls in system
 
