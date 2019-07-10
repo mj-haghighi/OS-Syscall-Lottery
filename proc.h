a3ce007c8252985a4805a425eb681e4af77622f5
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2019-07-10 21:10:25 +0430

    Add "ticket" to "proc"

--- proc.h
+++ proc.h
@@ -50,6 +50,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   int syscall_count[MAXSYSCALL];
+  int ticket;
 };
 
 // Process memory is laid out contiguously, low addresses first:
